b4ec737f00e8b3883dbd67431ae4169c166641f6
Alexander Timonenkov <nightblaze1@gmail.com>
2020-05-30 10:18:53 +0300

    Initial project setup

---
+++ TVShowManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TVShowManager
+//
+//  Created by Alexander Timonenkov on 30.05.2020.
+//  Copyright © 2020 ATi Soft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
